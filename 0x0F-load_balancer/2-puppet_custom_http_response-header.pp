9c3c3798cb57baf75ff84603c0d31ad1399ea6a1
Sammywens <nwene.samuel@gmail.com>
2024-03-19 14:25:23 -0700

    :white_check_mark: Shellcheck compliant

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,16 +1,20 @@
 # custom http header response NGiNX using puppet
 
-exec {'update':
+exec { 'update':
   command => '/usr/bin/apt-get update',
 }
--> package {'nginx':
+
+-> package { 'nginx':
   ensure => 'present',
 }
+
 -> file_line { 'http_header':
   path  => '/etc/nginx/nginx.conf',
   match => 'http {',
-  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+  line  => "add_header X-Served-By \"${hostname}\";",
 }
--> exec {'run2':
+
+-> exec { 'run2':
   command => '/usr/sbin/service nginx start',
 }
+
